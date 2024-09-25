e46d9a70fa02adacffae9540c51ac32c2617f132
RosaMeyer <jdc334@alumni.ku.dk>
2024-09-25 11:49:14 +0200

    formula.mli modification

--- src/formula.mli
+++ src/formula.mli
@@ -68,8 +68,9 @@ val quant_check: string -> t -> unit
 val fv: t -> (String.t, Base.String.comparator_witness) Base.Set.t
 val check_bindings: t -> bool
 
+(* TODO: Need to check subfs_dfs *)
 val subfs_dfs: t -> t list
-val subfs_dfs_regex: regex -> t list
+val subfs_dfs_regex: regex -> (t, regex) Base.Either.t list
 val subfs_scope: t -> int -> (int * (int list * int list)) list
 val preds: t -> t list
 val pred_names: t -> (string, Base.String.comparator_witness) Base.Set.t
