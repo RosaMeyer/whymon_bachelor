c3c12ac708e1127f80e44d3760b4f3c728e61494
RosaMeyer <jdc334@alumni.ku.dk>
2025-03-20 11:59:52 +0100

    comment

--- src/expl.ml
+++ src/expl.ml
@@ -1103,7 +1103,7 @@ module Pdt = struct
     | Leaf l -> l
     | _ -> raise (Invalid_argument "function not defined for nodes")
 
-  (* Added *)  
+  (* Added: *)  
   let is_Leaf = function
     | Leaf _ -> true
     | Node _ -> false
